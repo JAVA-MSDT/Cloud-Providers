9438cad52fe463f2079e834e9f93947fa90fbd48
Ahmed Samy <serenitydiver@hotmail.com>
2024-02-02 13:02:35 +0100

    Done implementing simple file upload to S3

--- mocks3/src/main/java/com/javamsdt/mocks3/MockS3Application.java
+++ mocks3/src/main/java/com/javamsdt/mocks3/MockS3Application.java
@@ -1,5 +1,6 @@
 package com.javamsdt.mocks3;
 
+import com.javamsdt.mocks3.service.SimpleS3Upload;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -11,6 +12,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 @RequiredArgsConstructor
 public class MockS3Application implements ApplicationRunner {
     private final S3Client s3Client;
+    private final SimpleS3Upload simpleS3Upload;
 
     public static void main(String[] args) {
         SpringApplication.run(MockS3Application.class, args);
@@ -20,5 +22,6 @@ public class MockS3Application implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("Testing S3Client Access:: ");
         s3Client.listBuckets().buckets().forEach(System.out::println);
+        System.out.println("simpleS3Upload:: " + simpleS3Upload.uploadFileToS3("S3File.txt"));
     }
 }
--- mocks3/src/main/java/com/javamsdt/mocks3/service/SimpleS3Upload.java
+++ mocks3/src/main/java/com/javamsdt/mocks3/service/SimpleS3Upload.java
@@ -1,11 +1,37 @@
 package com.javamsdt.mocks3.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Component
+@RequiredArgsConstructor
 public class SimpleS3Upload {
     @Value("${aws.s3.bucket.name}")
     private String bucketName;
+
+    private final S3Client s3Client;
+
+    public boolean uploadFileToS3(String fileName) throws IOException {
+        Path path = Path.of(fileName);
+        long fileSize = Files.size(path);
+        System.out.println("FileSize:: " + fileSize);
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build();
+        PutObjectResponse objectResponse = s3Client.putObject(objectRequest,
+                RequestBody.fromFile(path));
+        System.out.println("objectResponse:: " + objectResponse.responseMetadata().requestId());
+        return true;
+    }
 
 }
